9e652b9173a717d72b3a2499a7d6bae78b2515d8
leonardorosasilva <03466316022@edu.udesc.br>
2025-11-24 07:37:37 -0300

    Bean(add):: AdministradorBean

---
+++ src/beans/AdministradorBean.java
@@ -0,0 +1,22 @@
+package beans;
+
+public class AdministradorBean {
+    private int idAdministrador;
+    private int idUsuario;
+
+    public int getIdAdministrador() {
+        return idAdministrador;
+    }
+
+    public void setIdAdministrador(int idAdministrador) {
+        this.idAdministrador = idAdministrador;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+}
