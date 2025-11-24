b8a547ef9b705f7c13724205c829998496621fb3
leonardorosasilva <03466316022@edu.udesc.br>
2025-11-24 07:38:15 -0300

    Bean(add):: Cliente

---
+++ src/beans/ClienteBean.java
@@ -0,0 +1,13 @@
+package beans;
+
+public class ClienteBean {
+    private int idUsuario;
+
+    public int getidUsuario() {
+        return idUsuario;
+    }
+
+    public void setidUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+}
