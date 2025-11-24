ed54329ce894665bbeda20240a93186f9b4bc0a6
leonardorosasilva <03466316022@edu.udesc.br>
2025-11-24 07:16:54 -0300

    Bean(add):: Email model

---
+++ src/beans/Email.java
@@ -0,0 +1,31 @@
+package beans;
+
+public class Email {
+    private int idEmail;
+    private int idUsuario;
+    private String endereco;
+
+    public int getIdEmail() {
+        return idEmail;
+    }
+
+    public void setIdEmail(int idEmail) {
+        this.idEmail = idEmail;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
